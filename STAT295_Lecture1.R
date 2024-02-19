2bc24112cbd65ab842cea599306f81d886ac93e4
boltainn <tureonder@gmail.com>
2024-02-19 15:30:04 +0300

    --

--- STAT295_Lecture1.R
+++ STAT295_Lecture1.R
@@ -7,3 +7,13 @@ a
 
 now <- Sys.time()
 now
+
+#install.packages(c("devtools", "roxygen2", "testthat", "pkgdown","purr"))
+usethis::use_git_config(
+  user.name='boltainn',
+  user.email='tureonder@gmail.com'
+)
+usethis::create_github_token()
+gitcreds::gitcreds_set()
+
+getwd()
