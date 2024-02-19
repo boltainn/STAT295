7f1c9527ef7e8e2343d0ccf296fe6a9dcb594e27
boltainn <tureonder@gmail.com>
2024-02-19 15:56:50 +0300

    -

--- STAT295_Lecture1.R
+++ STAT295_Lecture1.R
@@ -7,13 +7,21 @@ a
 
 now <- Sys.time()
 now
+mil <- 10000000
+now-mil
+class(now)
 
 #install.packages(c("devtools", "roxygen2", "testthat", "pkgdown","purr"))
-usethis::use_git_config(
-  user.name='boltainn',
-  user.email='tureonder@gmail.com'
-)
-usethis::create_github_token()
-gitcreds::gitcreds_set()
+# usethis::use_git_config(
+#   user.name='boltainn',
+#   user.email='tureonder@gmail.com'
+# )
+# usethis::create_github_token()
+# gitcreds::gitcreds_set()
+# 
+# getwd()
 
-getwd()
+data(iris)
+str(iris)
+summary(iris)
+table(iris$Species)
